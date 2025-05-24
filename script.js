0498f5c8a3ee20e42d41f09557557ad8afad6863
Thomas Bradford <tsb004@gmail.com>
2025-05-24 14:33:36 -0500

    clean up images and make early modifications

---
+++ script.js
@@ -0,0 +1,24 @@
+// start of section for changing color of navbar based on the scroll position
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 100;
+    const sectionHeight = section.offsetHeight;
+
+    if (pageYOffset >= sectionTop && pageYOffset < sectionTop + sectionHeight) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+});
+// end of section for changing color of navbar based on the scroll position
